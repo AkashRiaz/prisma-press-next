94fae93d80526cc8b15c5e1a64bc528baa9d77b0
Md Akashuzzaman Riaz <akashriaz05@gmail.com>
2026-07-16 12:37:23 +0600

    Initial commit

---
+++ app/blogs/author/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthorPage = () => {
+    return (
+        <div>
+            this is the author page
+        </div>
+    );
+};
+
+export default AuthorPage;
---
+++ app/blogs/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogsPage = () => {
+  return (
+    <div>
+      this is the blogs page
+    </div>
+  );
+};
+
+export default BlogsPage;
